e1c8d4641b41131e8304a082c966d3bbf0221049
darkhorse-solution <darkhorse.solutionist@gmail.com>
2024-10-14 07:59:11 -0700

    Fix: deployed issue

--- app/api/uniswapV2/route.ts
+++ app/api/uniswapV2/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-  let request = await req.json();
+  const request = await req.json();
   
   return NextResponse.json({ message: 'Password reset successful', data: request }, { status: 200 });
 }
